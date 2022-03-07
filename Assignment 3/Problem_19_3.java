ab80484bc89d0914aa7192c7e80a3e0534f748fd
Kiran-512 <kiranyadav267@gmail.com>
2022-03-07 21:33:18 +0530

    Star pattern no 7 done

---
+++ Assignment 3/Problem_19_3.java
@@ -0,0 +1,30 @@
+class Problem_19_3 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k=0; k <i; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = 0; i < 4; i++) {
+
+            for (int k = 0; k <=i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print("*");
+            }
+            for (int j = 2; j >=i ; j--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
